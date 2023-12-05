652c74d0bbb3093c88ddf84c671892ddb63ec5b8
hanshuaishuai <houkhan@163.com>
2023-12-05 20:08:42 +0800

    恢复 README.md 文件

--- module_me/src/main/java/com/hzsoft/module/me/viewmodel/MainMeViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.hzsoft.module.me.viewmodel
-
-import androidx.lifecycle.SavedStateHandle
-import com.hzsoft.lib.base.mvvm.viewmodel.BaseViewModel
-
-class MainMeViewModel(val state: SavedStateHandle) : BaseViewModel()
---
+++ module_mine/src/main/java/debug/MeApplication.kt
@@ -0,0 +1,17 @@
+package debug
+
+import com.hzsoft.lib.base.module.ModuleApplication
+
+/**
+ *
+ *
+ * @author zhouhuan
+ * @Data 2020/12/3
+ */
+class MeApplication : ModuleApplication() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
