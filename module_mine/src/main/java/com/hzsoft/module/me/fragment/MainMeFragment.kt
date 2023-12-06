b02645a4e4f21a36d00ff8d93828f9a51c0440f1
hanshuaishuai <houkhan@163.com>
2023-12-06 12:26:19 +0800

    删除 lib_image_picker

--- module_mine/src/main/java/com/hzsoft/module/me/fragment/MainMeFragment.kt
+++ module_mine/src/main/java/com/hzsoft/module/me/fragment/MainMeFragment.kt
@@ -6,13 +6,11 @@ import com.google.gson.Gson
 import com.hzsoft.lib.base.utils.ToastUtil
 import com.hzsoft.lib.base.utils.ext.view.showToast
 import com.hzsoft.lib.base.view.BaseMvvmViewBindingFragment
-import com.hzsoft.lib.base.BaseApp
 import com.hzsoft.module.me.R
 import com.hzsoft.module.me.activity.RoomTestActivity
 import com.hzsoft.module.me.activity.SaveStateTestActivity
 import com.hzsoft.module.me.databinding.FragmentMeMainBinding
 import com.hzsoft.module.me.viewmodel.MainMeViewModel
-import com.ypx.imagepicker.demo.utils.ImagePickerHelper
 
 /**
  * Describe:
@@ -29,87 +27,21 @@ class MainMeFragment : BaseMvvmViewBindingFragment<FragmentMeMainBinding, MainMe
         }
     }
 
-
-    private var imagePickerHelper: ImagePickerHelper? = null
-
     override fun onBindLayout(): Int = R.layout.fragment_me_main
 
     override fun initView(mView: View) {
-        val config = ImagePickerHelper.with(ImagePickerHelper.Config())
-            .setWeChat(true)
-            .setMimeType(0)
-            .setShowOriginal(true)
-            .setSave(true)
-            .setMulti(true)
-            .setMaxCount(6)
-            .build()
-        imagePickerHelper = ImagePickerHelper(
-            activity, requireBinding().gridLayout,
-            { items ->
-                LogUtils.dTag(TAG, "选择的照片数据" + Gson().toJson(items))
-            }, config
-        )
+
     }
 
     override fun initData() {
 
     }
 
-    override fun initListener() {
-        requireBinding().button1.setOnClickListener(this::onClick)
-        requireBinding().button2.setOnClickListener(this::onClick)
-        requireBinding().button3.setOnClickListener(this::onClick)
-        requireBinding().button4.setOnClickListener(this::onClick)
-    }
-
     override fun enableToolbar(): Boolean = true
-
-    override fun getTootBarTitle(): String = getString(R.string.title_mine)
-
-    override fun onClick(v: View?) {
-        if (beFastClick()) {
-            return
-        }
-        when (v?.id) {
-            R.id.button_1 -> {
-                val config =
-                    ImagePickerHelper.with(ImagePickerHelper.Config())
-                        .setWeChat(true)
-                        .setMimeType(0)
-                        .setShowOriginal(true)
-                        .setSave(true)
-                        .setMulti(true)
-                        .build()
-                imagePickerHelper!!.setConfig(config)
-                "拍照模式设置成功".showToast()
-            }
-            R.id.button_2 -> {
-                val config =
-                    ImagePickerHelper.with(ImagePickerHelper.Config())
-                        .setWeChat(true)
-                        .setMimeType(1)
-                        .setShowOriginal(true)
-                        .setSave(true)
-                        .setMulti(true)
-                        .build()
-                imagePickerHelper!!.setConfig(config)
-                "视频模式设置成功".showToast()
-            }
-            R.id.button_3 -> {
-                val trim = requireBinding().editText.text.toString().trim()
-                if (trim.isBlank()) {
-                    ToastUtil.showToastCenter("输入内容不能为空")
-                    return
-                }
-                SaveStateTestActivity.start(mContext, trim)
-            }
-            R.id.button_4 -> {
-                RoomTestActivity.start(mContext)
-            }
-        }
-    }
 
     override fun initViewObservable() {
 
     }
+
+    override fun getTootBarTitle(): String = getString(R.string.title_mine)
 }
--- module_mine/src/main/java/debug/MainMeActivity.kt
+++ module_mine/src/main/java/debug/MainMeActivity.kt
@@ -7,13 +7,11 @@ import com.hzsoft.lib.base.utils.ToastUtil
 import com.hzsoft.lib.base.utils.ext.view.showToast
 import com.hzsoft.lib.base.view.BaseFragment
 import com.hzsoft.lib.base.view.BaseMvvmViewBindingActivity
-import com.hzsoft.lib.base.BaseApp
 import com.hzsoft.module.me.R
 import com.hzsoft.module.me.activity.RoomTestActivity
 import com.hzsoft.module.me.activity.SaveStateTestActivity
 import com.hzsoft.module.me.databinding.FragmentMeMainBinding
 import com.hzsoft.module.me.viewmodel.MainMeViewModel
-import com.ypx.imagepicker.demo.utils.ImagePickerHelper
 
 /**
  *
@@ -23,25 +21,10 @@ import com.ypx.imagepicker.demo.utils.ImagePickerHelper
  */
 class MainMeActivity : BaseMvvmViewBindingActivity<FragmentMeMainBinding, MainMeViewModel>() {
 
-    private var imagePickerHelper: ImagePickerHelper? = null
-
     override fun onBindLayout(): Int = R.layout.fragment_me_main
 
     override fun initView() {
-        val config = ImagePickerHelper.with(ImagePickerHelper.Config())
-            .setWeChat(true)
-            .setMimeType(0)
-            .setShowOriginal(true)
-            .setSave(true)
-            .setMulti(true)
-            .setMaxCount(6)
-            .build()
-        imagePickerHelper = ImagePickerHelper(
-            this, requireBinding().gridLayout,
-            { items ->
-                LogUtils.dTag(BaseFragment.TAG, "选择的照片数据" + Gson().toJson(items))
-            }, config
-        )
+
     }
 
     override fun initData() {
@@ -60,46 +43,7 @@ class MainMeActivity : BaseMvvmViewBindingActivity<FragmentMeMainBinding, MainMe
     override fun getTootBarTitle(): String = "Me"
 
     override fun onClick(v: View?) {
-        if (beFastClick()) {
-            return
-        }
-        when (v?.id) {
-            R.id.button_1 -> {
-                val config =
-                    ImagePickerHelper.with(ImagePickerHelper.Config())
-                        .setWeChat(true)
-                        .setMimeType(0)
-                        .setShowOriginal(true)
-                        .setSave(true)
-                        .setMulti(true)
-                        .build()
-                imagePickerHelper!!.setConfig(config)
-                "拍照模式设置成功".showToast()
-            }
-            R.id.button_2 -> {
-                val config =
-                    ImagePickerHelper.with(ImagePickerHelper.Config())
-                        .setWeChat(true)
-                        .setMimeType(1)
-                        .setShowOriginal(true)
-                        .setSave(true)
-                        .setMulti(true)
-                        .build()
-                imagePickerHelper!!.setConfig(config)
-                "视频模式设置成功".showToast()
-            }
-            R.id.button_3 -> {
-                val trim = requireBinding().editText.text.toString().trim()
-                if (trim.isBlank()) {
-                    ToastUtil.showToastCenter("输入内容不能为空")
-                    return
-                }
-                SaveStateTestActivity.start(mContext, trim)
-            }
-            R.id.button_4 -> {
-                RoomTestActivity.start(mContext)
-            }
-        }
+
     }
 
     override fun initViewObservable() {
